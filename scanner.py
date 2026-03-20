9a2e874c4393e05518f7f6a0a3fa4c2b42182fbf
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2026-03-20 11:55:50 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -5,3 +5,30 @@ from modules.secret_detector import detect_secrets
 from moodules.risk_engine import calculate_risks
 from report.html_report import generate_report
 from report.visualizer import create_graph
+
+def run_scan(domain): 
+  print(f"[+] Discovery assets for {domain} ...")
+  print(f"[+] Found {len(subdomains)} subdomains.")
+  
+  findings = {"public_bucket": False, "env_exposed": [], "secrets": []}
+  nodes = []
+
+  for sub in subdomains:
+    if check_s3(sub):
+      findings["public_bucket"] = True
+    env_files = check_env(sub)
+    if env_files:
+      findings["env_exposed"].extend(env_files)
+    for f in env_files:
+      content = "DB_PASSWORD=123"   # Placeholder for Demo
+      secrets = detecy_secrets(content)
+      findings["secrets"].extend(secrets)
+      nodes.append(("domain", sub))
+
+    risk_score = calculate_risk(findings)
+    generate_report({"domain": domain, "risk_score": risk_score, "findings": findings}, "report.html")
+    create_graph(nodes)
+
+print(f"[+] Scan complete! Risk Score:{risk_score}")
+print("[+] HTML report saved as report.html")
+print("[+] Attack surface graph saved as attack_surface.png")
