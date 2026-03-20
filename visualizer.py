f42d01587e4170aeb29f92470db658aa9dabad8a
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2026-03-20 11:04:11 +0000

    Delete visualizer.py

--- visualizer.py
+++
@@ -1,7 +0,0 @@
-from graphiz import Digraph
-
-def create_graph(nodes, filename="attack_surface"):
-  dot = Digraph()
-  for parent, child in nodes:
-    dot.edge(parent,child)
-  dot.render(filename, format='png')
